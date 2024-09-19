9d439c5f1eae9bdfff614fac49817907b7ed3d97
johnmotta <gabriel_motta31@hotmail.com>
2024-09-19 14:54:36 -0300

    Refactor: Consolidate API calls into single getMovie function

--- CineTrack/Service/ServiceManager.swift
+++ CineTrack/Service/ServiceManager.swift
@@ -11,73 +11,9 @@ class ServiceManager {
     
     static let shared = ServiceManager()
     
-    func getUpcommingMovie(completion: @escaping (Result<[Movie], NetworkError>) -> Void) {
-        let urlString = "\(Constants.baseURL)/3/movie/upcoming?api_key=\(Constants.API)&language=en-US&page=1"
+    func getMovie(segment: String, completion: @escaping (Result<[Movie], NetworkError>) -> Void) {
+        let urlString = "\(Constants.baseURL)/3/movie/\(segment)?api_key=\(Constants.API)&language=en-US&page=1"
         
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.invalidURL(urlString)))
-            return
-        }
-        
-        let session = URLSession.shared
-        
-        let task = session.dataTask(with: url) { data, _, error in
-            if let error {
-                completion(.failure(.networkFailure(error)))
-                return
-            }
-            
-            guard let data else {
-                completion(.failure(.noData))
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let result = try decoder.decode(MoviesResponse.self, from: data)
-                completion(.success(result.results))
-            } catch {
-                completion(.failure(.decodingError(error)))
-            }
-        }
-        task.resume()
-    }
-    
-    func getPopularMovie(completion: @escaping (Result<[Movie], NetworkError>) -> Void) {
-        let urlString = "\(Constants.baseURL)/3/movie/popular?api_key=\(Constants.API)&language=en-US&page=1"
-
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.invalidURL(urlString)))
-            return
-        }
-        
-        let session = URLSession.shared
-        
-        let task = session.dataTask(with: url) { data, _, error in
-            if let error {
-                completion(.failure(.networkFailure(error)))
-                return
-            }
-            
-            guard let data else {
-                completion(.failure(.noData))
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let result = try decoder.decode(MoviesResponse.self, from: data)
-                completion(.success(result.results))
-            } catch {
-                completion(.failure(.decodingError(error)))
-            }
-        }
-        task.resume()
-    }
-    
-    func getTopratedMovie(completion: @escaping (Result<[Movie], NetworkError>) -> Void) {
-        let urlString = "\(Constants.baseURL)/3/movie/top_rated?api_key=\(Constants.API)&language=en-US&page=1"
-
         guard let url = URL(string: urlString) else {
             completion(.failure(.invalidURL(urlString)))
             return
