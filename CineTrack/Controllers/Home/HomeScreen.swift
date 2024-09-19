ca547fba206305ce4f9038ca971f0b56177dc531
johnmotta <gabriel_motta31@hotmail.com>
2024-09-19 10:49:24 -0300

    Create HomeScreen

--- CineTrack/Controllers/Home/HomeScreen.swift
+++ CineTrack/Controllers/Home/HomeScreen.swift
@@ -5,4 +5,54 @@
 //  Created by John on 19/09/24.
 //
 
-import Foundation
+import UIKit
+
+class HomeScreen: UIView {
+    
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.createThreeColumnFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(MovieCollectionViewCell.self, forCellWithReuseIdentifier: MovieCollectionViewCell.identifier)
+        return collectionView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupCollectionView()
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupCollectionView() {
+        addSubview(collectionView)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: self.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        ])
+    }
+    
+    private func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = UIScreen.main.bounds.width
+        let padding: CGFloat = 16
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding * 2, left: 0, bottom: padding, right: 0)
+        flowLayout.minimumInteritemSpacing = minimumItemSpacing
+        flowLayout.itemSize = CGSize(width: itemWidth + 10, height: 210)
+        
+        return flowLayout
+    }
+
+}
