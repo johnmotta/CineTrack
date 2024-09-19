0778b05fbd952f1d87854f73a9fac125732436bf
johnmotta <gabriel_motta31@hotmail.com>
2024-09-19 13:45:06 -0300

    Add Service

---
+++ CineTrack/Service/ServiceManager.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceManager.swift
+//  CineTrack
+//
+//  Created by John on 19/09/24.
+//
+
+import Foundation
+
+class ServiceManager {
+    
+    static let shared = ServiceManager()
+    
+    func getUpcomming() {
+        
+    }
+}
