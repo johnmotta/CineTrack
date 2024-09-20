11a3ee62eed8d6e2c880151b76ece93bd656269d
johnmotta <gabriel_motta31@hotmail.com>
2024-09-20 14:06:42 -0300

    Add interactive favorite button with icon change

--- CineTrack/Controllers/FavoriteViewController.swift
+++ CineTrack/Controllers/FavoriteViewController.swift
@@ -11,7 +11,5 @@ class FavoriteViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .purple
     }
 }
--- CineTrack/View/MovieCollectionViewCell.swift
+++ CineTrack/View/MovieCollectionViewCell.swift
@@ -10,9 +10,20 @@ import SDWebImage
 
 class MovieCollectionViewCell: UICollectionViewCell {
     
+    var isFavorited = false
+    
     static let identifier: String = String(describing: MovieCollectionViewCell.self)
     
     private let imageView = ImageDefault()
+    
+    private let favoriteButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "star"), for: .normal)
+        button.tintColor = .systemGray
+        button.addTarget(self, action: #selector(favoriteTapped), for: .touchUpInside)
+        return button
+    }()
     
     private let nameLabel = LabelDefault(fontSize: 12, fontWeight: .regular, numberOfLines: 2)
     
@@ -29,6 +40,7 @@ class MovieCollectionViewCell: UICollectionViewCell {
     
     private func addElements() {
         contentView.addSubview(imageView)
+        contentView.addSubview(favoriteButton)
         contentView.addSubview(nameLabel)
     }
     
@@ -40,14 +52,21 @@ class MovieCollectionViewCell: UICollectionViewCell {
             imageView.heightAnchor.constraint(equalToConstant: 120)
         ]
         
+        let favoriteButtonConstraints = [
+            favoriteButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 4),
+            favoriteButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -4),
+            favoriteButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ]
+        
         let nameLabelConstraints = [
             nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 4),
             nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            nameLabel.trailingAnchor.constraint(equalTo: favoriteButton.leadingAnchor),
             nameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ]
         
         NSLayoutConstraint.activate(imageViewConstraints)
+        NSLayoutConstraint.activate(favoriteButtonConstraints)
         NSLayoutConstraint.activate(nameLabelConstraints)
     }
     
@@ -72,5 +91,14 @@ class MovieCollectionViewCell: UICollectionViewCell {
             nameLabel.text = "None"
         }
     }
-
+    
+    @objc func favoriteTapped() {
+        isFavorited.toggle()
+        
+        let imageName = isFavorited ? "star.fill" : "star"
+        let tintColor = isFavorited ? UIColor.systemYellow : UIColor.gray
+        
+        favoriteButton.setImage(UIImage(systemName: imageName), for: .normal)
+        favoriteButton.tintColor = tintColor
+    }
 }
