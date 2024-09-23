faa8fe9877aa9f460a61f5f6caf0b516ef7fd77d
johnmotta <gabriel_motta31@hotmail.com>
2024-09-23 14:27:55 -0300

    Create FavoriteViewController to sync favorites with HomeViewController

--- CineTrack/Controllers/FavoriteViewController.swift
+++ CineTrack/Controllers/FavoriteViewController.swift
@@ -9,7 +9,71 @@ import UIKit
 
 class FavoriteViewController: UIViewController {
     
+    var viewModel: HomeViewModel
+    
+    private let favoriteFeedTable: UITableView = {
+        let table = UITableView()
+        table.register(FavoriteTableViewCell.self, forCellReuseIdentifier: FavoriteTableViewCell.identifier)
+        return table
+    }()
+    
+    
+    init(viewModel: HomeViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(favoriteFeedTable)
+        favoriteFeedTable.delegate = self
+        favoriteFeedTable.dataSource = self
+        
+        reloadFavorites()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        reloadFavorites()
+    }
+    
+    private func reloadFavorites() {
+        DispatchQueue.main.async {
+            self.favoriteFeedTable.reloadData()
+        }
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        favoriteFeedTable.frame = view.bounds
+    }
+}
+
+extension FavoriteViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.favoriteMovies?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteTableViewCell.identifier, for: indexPath) as? FavoriteTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        guard let movie = viewModel.favoriteMovies?[indexPath.row] else { return UITableViewCell() }
+        cell.onFavoriteTapped = { [weak self] in
+            guard let self = self else { return }
+            self.viewModel.toggleFavorite(movie: movie)
+            tableView.reloadData()
+        }
+        cell.configure(with: movie)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
     }
 }
