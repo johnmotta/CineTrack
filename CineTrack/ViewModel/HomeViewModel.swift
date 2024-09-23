7ca73e1e849f397f30f51fbebe252e6198b60b57
johnmotta <gabriel_motta31@hotmail.com>
2024-09-23 14:29:00 -0300

    Update HomeViewModel to handle movie favorites

--- CineTrack/ViewModel/HomeViewModel.swift
+++ CineTrack/ViewModel/HomeViewModel.swift
@@ -8,8 +8,16 @@
 import UIKit
 
 class HomeViewModel {
-    var movie: [Movie]?
+    var movies: [Movie]?
+    var favoriteMovies: [Movie]?
     var section = Sections.upcoming
+
+    func toggleFavorite(movie: Movie) {
+        if let index = movies?.firstIndex(where: { $0.id == movie.id }) {
+            movies?[index].isFavorite?.toggle()
+            favoriteMovies = movies?.filter { $0.isFavorite == true }
+        }
+    }
     
     func fetchData(_ collectionView: UICollectionView) {
         switch self.section {
@@ -26,12 +34,15 @@ class HomeViewModel {
         ServiceManager.shared.getMovie(segment: segment) { [weak self] result in
             guard let self = self else { return }
             switch result {
-            case .success(let success):
-                self.movie = success
+            case .success(let movies):
+                self.movies = movies.map {
+                    var mutableMovie = $0
+                    mutableMovie.isFavorite = false
+                    return mutableMovie
+                }
                 DispatchQueue.main.async {
                     collectionView.reloadData()
                 }
-
             case .failure(let failure):
                 print(failure)
             }
