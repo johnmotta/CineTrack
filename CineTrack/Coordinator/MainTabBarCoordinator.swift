8439f701c490a18b273480dc7a8e0347b796a571
johnmotta <gabriel_motta31@hotmail.com>
2024-09-24 09:46:42 -0300

    Integrate SearchCoordinator in MainTabBarCoordinator

--- CineTrack/Coordinator/MainTabBarCoordinator.swift
+++ CineTrack/Coordinator/MainTabBarCoordinator.swift
@@ -18,14 +18,19 @@ class MainTabBarCoordinator: Coordinator {
     
     func start() {
         let tabBarController = MainTabViewController()
+        
         let homeCoordinator = HomeCoordinator(navigationController: UINavigationController(), viewModel: viewModel)
         homeCoordinator.start()
+        
+        let searchCoordinator = SearchCoordinator(navigationController: UINavigationController())
+        searchCoordinator.start()
         
         let favoriteCoordinator = FavoriteCoordinator(navigationController: UINavigationController(), viewModel: viewModel)
         favoriteCoordinator.start()
         
         tabBarController.viewControllers = [
             homeCoordinator.navigationController,
+            searchCoordinator.navigationController,
             favoriteCoordinator.navigationController
         ]
         
