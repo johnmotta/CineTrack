5e480c46e73ff873e074fc9997cbcac23538082b
John Motta <johnmotta@MacBook-Pro-de-John.local>
2024-10-08 15:09:49 -0300

    Update DetailViewController to hide tab bar on view appearance

--- CineTrack/Controllers/DetailViewController.swift
+++ CineTrack/Controllers/DetailViewController.swift
@@ -17,10 +17,22 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
     }
     
-    func configure(movie: Movie) {
-        detailView.configure(with: movie)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
+
+    
+    func configure(movie: Movie, cast: [Cast]) {
+        detailView.configure(with: movie, cast: cast)
     }
 }
 
